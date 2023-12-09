2af4ddaabfeabddc17793fce47e92a67fef8c3fb
Mario Todorov <mtodorov95@gmail.com>
2023-12-09 08:31:18 +0200

    Remove harpoon

--- lua/mario/packer.lua
+++ lua/mario/packer.lua
@@ -15,7 +15,6 @@ return require('packer').startup(function(use)
   vim.cmd('colorscheme rose-pine')
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('nvim-treesitter/playground')
-  use('ThePrimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
   use {
